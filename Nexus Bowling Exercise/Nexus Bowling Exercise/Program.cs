d6d1e39f28601caab78d8fece098bda16e5dde99
Cordell Wagendorf <cordell.wagendorf@ndus.edu>
2016-05-24 09:22:11 -0500

    Commit time

--- Nexus Bowling Exercise/Nexus Bowling Exercise/Program.cs
+++ Nexus Bowling Exercise/Nexus Bowling Exercise/Program.cs
@@ -8,8 +8,56 @@ namespace Nexus_Bowling_Exercise
 {
     class Program
     {
+        static void GameLoop (Scoreboard scoreboard, Random rand)
+        {
+            while (scoreboard.frameNumber < 10)
+            {
+                Console.Clear();
+
+                // Calc throws
+                int[] throws = new int[2];
+                throws[0] = rand.Next(11);
+                throws[1] = rand.Next(11 - throws[0]);
+
+                Console.WriteLine("Frame: " + (scoreboard.frameNumber + 1));
+                Console.WriteLine();
+
+                scoreboard.RecordFrame(throws);
+
+                // Scoreboard
+                Console.WriteLine("\t| 1  |\t| 2  |\t| 3  |\t| 4  |\t| 5  |\t| 6  |\t| 7  |\t| 8  |\t| 9  |\t| 10 |");
+                Console.WriteLine("        ------------------------------------------------------------------------------");
+                for (int i = 0; i < 10; i++)
+                    Console.Write("\t| " + scoreboard.frameTotals[i] + "  |");
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Throw #1: " + throws[0] + "\t" + "Throw #2: " + throws[1]);
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Total score: " + scoreboard.Score);
+
+                Console.ReadLine();
+            }
+
+            //Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Welcome to the Bowling Game!!!");
+            Console.WriteLine("Press any key to start the game");
+
+            Console.ReadLine();
+
+            Scoreboard scoreboard = new Scoreboard();
+            Random rand = new Random(DateTime.Now.Millisecond);
+
+            GameLoop(scoreboard, rand);
         }
     }
 }
---
+++ Nexus Bowling Exercise/Nexus Bowling Exercise/Scoreboard.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nexus_Bowling_Exercise
+{
+    class Scoreboard : ISimpleBowlingGame
+    {
+        // Scoring variables
+        public int[] frameTotals = new int[10];
+
+        // Tracking
+        public int frameNumber;
+        private int[][] throwTracker = new int[10][];
+
+        // Throw counters
+        int[] scoresStrike = new int[3];
+        int[] scoresSpare = new int[2];
+        int throwsStrike;
+        int throwsSpare;
+
+        // Special Tracking
+        
+
+        // Constructor
+        public Scoreboard ()
+        {
+            // Set frames
+            frameNumber = 0;
+        }
+
+        public void RecordFrame(params int[] throws)
+        {
+            throwTracker[frameNumber] = throws;
+            
+            frameNumber++;
+        }
+
+        public int Score
+        {
+            get
+            {
+                for (int i = 0; i < frameNumber; i++)
+                {
+                     // Check for strike
+                     if (throwTracker[frameNumber][0] == 10)
+                    {
+                        int strikeScore = 0;
+
+                        // Add next two throws to score
+                    }
+
+                    // Check for spare
+                    if ((throwTracker[frameNumber][0] + throwTracker[frameNumber][1]) == 10)
+                    {
+
+
+                        // Add next throw
+                    }
+
+                    // Add pins
+                    frameTotals[frameNumber] = throwTracker[frameNumber][0] + throwTracker[frameNumber][1];
+                }
+
+                return frameTotals.Sum();
+            }
+        }
+    }
+}
